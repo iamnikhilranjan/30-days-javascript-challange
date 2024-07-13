5b19110c64714326c3f507ffa806c75ba11a700c
iamnikhilranjan <nikhil21907@gmail.com>
2024-07-13 13:21:27 +0530

    Solved day 01

---
+++ 01_Variables-and-data-types.js
@@ -0,0 +1,35 @@
+//Variable Declaration
+var x = 19;
+console.log(x);
+
+let y = "July";
+console.log(y);
+
+//Constant Declaration
+const c = true;
+console.log(c);
+
+//Data types
+let num = 1907;
+let str = "july";
+let bool = false;
+const obj = {};
+const arr = [];
+
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(bool));
+console.log(typeof(obj));
+console.log(typeof(arr));
+
+//Resigning Variables
+let a = 1;
+console.log(a);
+a = 12;
+console.log(a);
+
+//unnderstanding const
+const b = 23;
+b = 4;
+console.log(b);
+
